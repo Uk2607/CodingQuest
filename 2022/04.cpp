70f7385b43e9fca379fef5383ad25cb3ef745721
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-03-29 22:55:20 +0530

    2022 d4

--- 2022/04.cpp
+++ 2022/04.cpp
@@ -30,8 +30,43 @@ vector<string>  get_input(string file_name) {
     return arr;
 }
 
+int gameStatus(vector<vector<int>>grid) {
+    for(int i=0;i<7;i++) {
+        for(int j=0;j<7;j++) {  // Forward, Downward & Main Diagonal
+            int x = grid[i][j];
+            if(x==-1) continue;
+            if(i<4 && grid[i+1][j]==x && grid[i+2][j]==x && grid[i+3][j]==x) return x;  // Forward
+            if(j<4 && grid[i][j+1]==x && grid[i][j+2]==x && grid[i][j+3]==x) return x;  // Downward
+            if(i<4 && j<4 && grid[i+1][j+1]==x && grid[i+2][j+2]==x && grid[i+3][j+3]==x) return x;  // Diag
+        }
+        for(int j=6;j>=3;j--) {  // Anti Diagonal
+            int x = grid[i][j];
+            if(x==-1) continue;
+            if(i<4 && grid[i+1][j-1]==x && grid[i+2][j-2]==x && grid[i+3][j-3]==x) return x;  // Anti Diag
+        }
+    }
+    return -1;
+}
+
 void solve(vector<string>&arr) {
-    cout<<"\nRES:: "<<""<<"\n\n";
+    vector<int>pFreq(3, 0);
+    for(string game: arr) {
+        vector<vector<int>>grid(7, vector<int>(7, -1));
+        int player = 0;
+        vector<int>pos(7, 0);
+        for(int i=0;i<game.length();i++) {
+            int j = pos[game[i]-'1'];
+            grid[game[i]-'1'][j] = player;
+            int won = gameStatus(grid);
+            pos[game[i]-'1']++;
+            player = (player+1)%3;
+            if(won!=-1) {
+                pFreq[won]++;
+                break;
+            }
+        }
+    }
+    cout<<"\nRES:: "<<pFreq[0]*pFreq[1]*pFreq[2]<<"\n\n";
 }
 
 int main() {

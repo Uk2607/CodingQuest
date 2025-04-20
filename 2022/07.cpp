cdff1148f8c22caea2063fac31e4216c01088d15
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-04-20 09:23:33 +0530

    d7

--- 2022/07.cpp
+++ 2022/07.cpp
@@ -36,7 +36,31 @@ vector<vector<int>>  get_input(string file_name) {
 }
 
 void solve(vector<vector<int>>&arr) {
-    cout<<"\nRES:: "<<""<<"\n\n";
+    int R = 20000, C = 100000;
+    vector<vector<int>>pre(R+1, vector<int>(C+1, 0)), GRID(R, vector<int>(C, 0));
+    int cnt = 0;
+    for(vector<int>row: arr) {
+        int x = row[0], y = row[1];
+        int r = row[2], c = row[3];
+        pre[x][y]+=1;
+        if(x+r<=R) pre[x+r][y]-=1;
+        if(y+c<=C) pre[x][y+c]-=1;
+        if(x+r<=R && y+c<=C) pre[x+r][y+c]+=1;
+    }
+    for(int i=0;i<R;i++) {
+        for(int j=0;j<C;j++) {
+            if(i>0) pre[i][j] += pre[i-1][j];
+            if(j>0) pre[i][j] += pre[i][j-1];
+            if(i>0 && j>0) pre[i][j] -= pre[i-1][j-1];
+            if(pre[i][j]==0) cnt++;
+        }
+    }
+    // for(int i=0;i<R;i++)
+    //     for(int j=0;j<C;j++)
+    //         GRID[i][j] = pre[i][j];
+
+    cout<<"\nRES:: "<<cnt<<"\n\n";
+    return;
 }
 
 int main() {

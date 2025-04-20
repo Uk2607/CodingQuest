dc78df2aca4bc96f0dc96dcf891bf61ec83e6187
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-04-20 11:31:18 +0530

    d2 working

--- 2023/02.cpp
+++ 2023/02.cpp
@@ -27,9 +27,30 @@ vector<int> get_input(string file_name) {
     return arr;
 }
 
+string to_bin(int x) {
+    string b = "";
+    while(x>0) {
+        b = (x%2==1?'1':'0') + b;
+        x>>=1;
+    }
+    while (b.length()<16) b = '0'+b;
+    return b;
+}
+
 void solve(vector<int>&arr) {
-    for(int x: arr) cout<<x<<" ";
-    cout<<"\nRES:: "<<""<<"\n\n";
+    int sum = 0, cnt = 0;
+    for(int x: arr) {
+        string bin_str = to_bin(x);
+        cout<<x<<"\t:: "<<bin_str;
+        if(bin_str[0]=='1') {
+            int y = x-32768; // 32768 = 2^15 remove parity bit
+            cout<<" : "<<y;
+            sum += y;
+            cnt++;
+        }
+        cout<<"\n";
+    }
+    cout<<"\nRES:: "<<sum/cnt<<"\n\n";
     return;
 }
 

c86eeb647b0c3b58727fb8af35d5afc9e0d9122a
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-04-22 04:18:12 +0530

    d4

--- 2023/04.cpp
+++ 2023/04.cpp
@@ -27,8 +27,55 @@ vector<string> get_input(string file_name) {
     return arr;
 }
 
+int hex_to_dec(string hx) {
+    int mul = 0, dec = 0;
+    for(int i=hx.length()-1;i>=0;i--) {
+        int x;
+        if(hx[i]>='0' && hx[i]<='9') x = hx[i]-'0';
+        else x = hx[i]-'a'+10;
+        dec += (pow(16, mul)*x);
+        mul++;
+    }
+    return dec;
+}
+
+string dec_to_hex(int dec) {
+    if (dec == 0) return "0"; // Handle the case for 0 explicitly
+    string hx = "";
+    while (dec > 0) {
+        int r = dec % 16;
+        hx = (r<10?to_string(r):string(1, char(r-10+'a'))) + hx;
+        dec /= 16;
+    }
+    while(hx.length()<2) hx = "0"+hx;
+    return hx;
+}
+
 void solve(vector<string>&arr) {
-    cout<<"\nRES:: "<<""<<"\n\n";
+    map<int, string>mp;
+    for(string str: arr) {
+        if(str.substr(0, 4)!="5555") continue;
+        string hex_str = str.substr(4, 8); // ship number
+        string seq_no = str.substr(12, 2);
+        string checksum = str.substr(14, 2);
+        string payload = str.substr(16);
+        int sum = 0;
+        for(int i=0;i<payload.length();i+=2) {
+            string hx = payload.substr(i, 2);
+            int x = hex_to_dec(hx);
+            sum+=x;
+        }
+        sum %= 256;
+        if(checksum!=dec_to_hex(sum)) continue;
+        mp[stoi(seq_no)] = payload;
+    }
+    string res = "", msg = "";
+    for(auto it: mp) msg+=it.second;
+    for(int i=0;i<msg.length();i+=2) {
+        int x = hex_to_dec(msg.substr(i, 2));
+        res.push_back(x);
+    }
+    cout<<"\nRES:: "<<res<<"\n\n";
     return;
 }
 

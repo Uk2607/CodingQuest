8eca84664dd2e93fe8c2b3396a0571383da62536
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-04-27 14:27:50 +0530

    moces

--- 2023/07.cpp
+++ 2023/07.cpp
@@ -37,7 +37,7 @@ pair<vector<pair<int,int>>, string> get_input(string file_name) {
                     if(flag) y = stoi(t);
                     flag = !flag;
                 }
-                arr.push_back({x, y});
+                arr.push_back({y, x});
             }
         } else {
             moves = line;
@@ -49,7 +49,7 @@ pair<vector<pair<int,int>>, string> get_input(string file_name) {
 }
 
 void solve(vector<pair<int,int>>&fruits, string &moves) {
-    int idx = 0, N = 20, score;
+    int idx = 0, N = 20, score, k = 0;
     vector<pair<int,int>>snake = {{0, 0}};
     pair<int,int>f = fruits[idx];;
     vector<string>grid(N, string(N, '.'));
@@ -57,30 +57,38 @@ void solve(vector<pair<int,int>>&fruits, string &moves) {
     grid[f.first][f.second] = 'F';
     for(char m: moves) {
         int snake_len = snake.size(), nx, ny;
-        if(m == 'R') { // right
-            nx = snake[0].first, ny = snake[0].second+1;
-        } else if(m == 'L') { // left
-            nx = snake[0].first, ny = snake[0].second-1;
-        } else if(m == 'U') { // up
-            nx = snake[0].first+1, ny = snake[0].second;
-        } else { // down
-            nx = snake[0].first-1, ny = snake[0].second;
-        }
+
+        if(m == 'R') nx = snake[0].first, ny = snake[0].second+1;
+        else if(m == 'L') nx = snake[0].first, ny = snake[0].second-1;
+        else if(m == 'U') nx = snake[0].first-1, ny = snake[0].second;
+        else nx = snake[0].first+1, ny = snake[0].second;
+
         if(nx<0 || nx>=N || ny<0 || ny>=N) break;
         if(make_pair(nx, ny) != f) {
             grid[snake[snake_len-1].first][snake[snake_len-1].second] = '.';
-            for(int i=snake_len-1;i>0;i--) snake[i] = snake[i-1];
         }
         else {
             score+=100;
+            snake.push_back({-1,-1});
             idx++;
             f = fruits[idx];
             grid[f.first][f.second] = 'F';
         }
+        for(int i=snake.size()-1;i>0;i--) snake[i] = snake[i-1];
         for(pair<int,int>p: snake) if(p==make_pair(nx, ny)) break;
         snake[0] = {nx, ny};
-        for(pair<int,int>p: snake) grid[p.first][p.second] = 'S';
+        for(pair<int,int>p: snake) grid[p.first][p.second] = 'o';
+        grid[snake[0].first][snake[0].second] = 'O';
         score++;
+
+        cout<<m<<"\n";
+        for(string r: grid) {
+            cout<<r<<"\n";
+            cout<<"\n";
+        } cout<<"\n";
+
+        cout<<++k<<" : "<<nx<<", "<<ny<<" == "<<score<<"\n";
+
     }
     cout<<"\nRES::\n"<<score<<"\n";
 }

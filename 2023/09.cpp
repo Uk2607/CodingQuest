a7f61040a7346c1045da5828a1783610810a661c
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-04-28 12:18:56 +0530

    struct created

--- 2023/09.cpp
+++ 2023/09.cpp
@@ -26,7 +26,40 @@ vector<string> get_input(string file_name) {
     return arr;
 }
 
+struct TreeNode {
+    long long id;
+    TreeNode *left, *right;
+
+    TreeNode(long long id) {
+        left = NULL;
+        right = NULL;
+    }
+
+    TreeNode(TreeNode *node) {
+        id = node->id;
+        left = node->left;
+        right = node->right;
+    }
+};
+
+long long hex_to_dec(string hex) {
+    int mul = 0;
+    long long res = 0;
+    for(int i=hex.length()-1;i>=0;i--) {
+        int x;
+        if(hex[i]>='a' && hex[i]<='f') x = hex[i]-'a'+10;
+        else x = hex[i]-'0';
+        res += pow(16, mul)*x;
+        mul++;
+    }
+    return res;
+}
+
 void solve(vector<string>&arr) {
+    for(string hex: arr) {
+        long long x = hex_to_dec(hex);
+        cout<<hex<<" "<<x<<"\n";
+    }
     cout<<"RES:: "<<""<<"\n";
 }
 

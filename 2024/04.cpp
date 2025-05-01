bf160dcd0f075dc08e3c17cd84f79af9b0196b17
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-05-01 13:32:00 +0530

    d4 ip

--- 2024/04.cpp
+++ 2024/04.cpp
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-vector<vector<string>> get_input(string file_name) {
+vector<vector<float>> get_input(string file_name) {
     string file_path = "2024/"+file_name+".in";
     ifstream file(file_path);
 
-    vector<vector<string>> arr;
+    vector<vector<float>> arr;
 
     if (!file.is_open()) {
         cerr << "Failed to open file: " << file_path << endl;
@@ -18,26 +18,33 @@ vector<vector<string>> get_input(string file_name) {
     }
 
     string line;
+    bool first = true;
 
     while (getline(file, line)) {
+        if(first) {
+            first = false;
+            continue;
+        }
         vector<string>t;
         stringstream ss(line);
         string x;
         while(ss>>x) t.push_back(x);
-        arr.push_back(t);
+        arr.push_back({stof(t[t.size()-3]), stof(t[t.size()-2]), stof(t[t.size()-1])});
     }
     
     file.close();
     return arr;
 }
 
-void solve(vector<vector<string>>&arr) {
+void solve(vector<vector<float>>&arr) {
+    for(vector<float> f: arr)
+        cout<<f[0]<<", "<<f[1]<<", "<<f[2]<<"\n";
     cout<<"\nRES:: "<<""<<"\n\n";
     return;
 }
 
 int main() {
-    vector<vector<string>> arr = get_input("04");
+    vector<vector<float>> arr = get_input("04");
     solve(arr);
     return 0;
 }

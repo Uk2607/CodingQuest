044b9a2cbc79d69b3c2c4b1481b74336079fe54c
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-05-01 13:23:31 +0530

    d3

--- 2024/03.cpp
+++ 2024/03.cpp
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-vector<vector<string>> get_input(string file_name) {
+vector<int> get_input(string file_name) {
     string file_path = "2024/"+file_name+".in";
     ifstream file(file_path);
 
-    vector<vector<string>> arr;
+    vector<int> arr;
 
     if (!file.is_open()) {
         cerr << "Failed to open file: " << file_path << endl;
@@ -20,24 +20,39 @@ vector<vector<string>> get_input(string file_name) {
     string line;
 
     while (getline(file, line)) {
-        vector<string>t;
         stringstream ss(line);
-        string x;
-        while(ss>>x) t.push_back(x);
-        arr.push_back(t);
+        int x;
+        while(ss>>x) arr.push_back(x);
     }
     
     file.close();
     return arr;
 }
 
-void solve(vector<vector<string>>&arr) {
-    cout<<"\nRES:: "<<""<<"\n\n";
+void solve(vector<int>arr) {
+    int R = 80, C = 100, idx = 0;
+    bool dot = true;
+    vector<string>grid(R, string(C, '.'));
+    for(int i=0;i<R;i++) {
+        for(int j=0;j<C;j++) {
+            if(arr[idx]<=0) {
+                idx++;
+                dot =! dot;
+            }
+            if(idx==arr.size()) break;
+            if(dot) grid[i][j] = '.';
+            else grid[i][j] = '#';
+            arr[idx]--;
+        }
+        if(idx==arr.size()) break;
+    }
+    cout<<"\nRES::\n";
+    for(string str: grid) cout<<str<<"\n";
     return;
 }
 
 int main() {
-    vector<vector<string>> arr = get_input("03");
+    vector<int> arr = get_input("03");
     solve(arr);
     return 0;
 }

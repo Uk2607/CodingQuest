6849584106ac43783373f3c9b2f0e33f6c1ea3c3
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-05-01 11:52:37 +0530

    d10

--- 2023/10.cpp
+++ 2023/10.cpp
@@ -3,6 +3,7 @@
 #include<fstream>
 #include<sstream>
 #include<map>
+#include<set>
 
 using namespace std;
 
@@ -38,15 +39,29 @@ map<string, vector<pair<int, string>>> get_input(string file_name) {
     return mp;
 }
 
-void solve(map<string, vector<pair<int, string>>>&mp) {
+void solve(map<string, vector<pair<int, string>>>&graph) {
     string src = "TYC", dest = "EAR";
     int stop_time = 600;
-    for(auto it: mp) {
-        cout<<it.first<<" => ";
-        for(auto [cost, st]: it.second) cout<<st<<":"<<cost<<" ";
-        cout<<"\n";
+    set<pair<int,string>>pq;
+    map<string,int>distance;
+    for(auto node: graph) distance[node.first] = INT_MAX;
+    distance[src] = 0;
+    pq.insert({0, src});
+    while(!pq.empty()) {
+        auto [curr_dist, curr_node] = *pq.begin();
+        pq.erase(pq.begin());
+        for(auto [weight, next_node]: graph[curr_node]) {
+            int dist = curr_dist+weight+stop_time;
+            if(dist<distance[next_node]) {
+                pq.erase({distance[next_node], next_node});
+                distance[next_node] = dist;
+                pq.insert({dist, next_node});
+            }
+        }
     }
-    cout<<"RES:: "<<""<<"\n";
+    for (const auto& pair : distance)
+        cout << "Distance from " << src << " to " << pair.first << " is " << pair.second-600 << endl;
+    cout<<"RES:: "<<distance[dest]-600<<"\n";
 }
 
 int main() {

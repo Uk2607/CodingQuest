5e3f9927f885c6f9f959d177a107c25f47287249
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-05-01 16:54:57 +0530

    grid created

--- 2024/06.cpp
+++ 2024/06.cpp
@@ -6,38 +6,35 @@
 
 using namespace std;
 
-vector<vector<string>> get_input(string file_name) {
-    string file_path = "2024/"+file_name+".in";
-    ifstream file(file_path);
-
-    vector<vector<string>> arr;
-
-    if (!file.is_open()) {
-        cerr << "Failed to open file: " << file_path << endl;
-        return arr;
+void solve(string key, string msg) {
+    vector<string>grid(5, string(5, '.'));
+    vector<bool>flag(26, false);
+    flag[9] = true; // j changed to i
+    int idx = 0, idx2 = 0;
+    for(int i=0;i<5;i++) {
+        for(int j=0;j<5;j++) {
+            if(idx<key.length()) {
+                while(idx<key.length() && flag[key[idx]-'a']) idx++;
+                grid[i][j] = key[idx];
+                flag[key[idx]-'a'] = true;
+                idx++;
+            } else {
+                while(idx2<26 && flag[idx2]) idx2++;
+                grid[i][j] = idx2+'a';
+                flag[idx2] = true;
+                idx2++;
+            }
+        }
     }
-
-    string line;
-
-    while (getline(file, line)) {
-        vector<string>t;
-        stringstream ss(line);
-        string x;
-        while(ss>>x) t.push_back(x);
-        arr.push_back(t);
-    }
-    
-    file.close();
-    return arr;
-}
-
-void solve(vector<vector<string>>&arr) {
+    for(string str: grid) cout<<str<<"\n";
     cout<<"\nRES:: "<<""<<"\n\n";
     return;
 }
 
 int main() {
-    vector<vector<string>> arr = get_input("06");
-    solve(arr);
+    // Get the input from the file 06.in
+    string key = "codingquest";
+    string msg = "rmqfgs yegv em qnpu pdml dc atuy olzy anpu";
+    solve(key, msg);
     return 0;
 }

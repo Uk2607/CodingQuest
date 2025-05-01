8687556159660ca4edfe4aae38a13c2ae0c8eafd
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-05-01 17:54:20 +0530

    d6

--- 2024/06.cpp
+++ 2024/06.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void solve(string key, string msg) {
+vector<string> get_grid(string key) {
     vector<string>grid(5, string(5, '.'));
     vector<bool>flag(26, false);
     flag[9] = true; // j changed to i
@@ -26,8 +26,38 @@ void solve(string key, string msg) {
             }
         }
     }
-    for(string str: grid) cout<<str<<"\n";
-    cout<<"\nRES:: "<<""<<"\n\n";
+    return grid;
+}
+
+void solve(string key, string msg) {
+    vector<string>grid = get_grid(key);
+
+    vector<pair<int,int>>mp(26, {-1, -1});
+    for(int i=0;i<grid.size();i++)
+        for(int j=0;j<grid[0].length();j++)
+            mp[grid[i][j]-'a'] = {i, j};
+    
+    string res = "";
+    for(int i=0;i<msg.length();i++) {
+        if(msg[i]==' ') {
+            res+=' ';
+            continue;
+        }
+        string str = msg.substr(i, 2);
+        int x1 = mp[str[0]-'a'].first, y1 = mp[str[0]-'a'].second, x2 = mp[str[1]-'a'].first, y2 = mp[str[1]-'a'].second;
+        if(x1!=x2 && y1!=y2) {
+            res += grid[x1][y2];
+            res += grid[x2][y1];
+        } else if(x1==x2 && y1!=y2) {
+            res += grid[x1][(y1+4)%5];
+            res += grid[x2][(y2+4)%5];
+        } else if(x1!=x2 && y1==y2) {
+            res += grid[(x1+4)%5][y1];
+            res += grid[(x2+4)%5][y2];
+        }
+        i++;
+    }
+    cout<<"\nRES:: "<<res<<"\n\n";
     return;
 }
 

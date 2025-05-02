230320e4cb5deadf6be8a99871df0c4c35f4c1a0
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-05-02 10:22:22 +0530

    d9 ip

--- 2024/09.cpp
+++ 2024/09.cpp
@@ -6,38 +6,40 @@
 
 using namespace std;
 
-vector<vector<string>> get_input(string file_name) {
+pair<vector<string>,vector<string>> get_input(string file_name) {
     string file_path = "2024/"+file_name+".in";
     ifstream file(file_path);
 
-    vector<vector<string>> arr;
+    vector<string>arr1, arr2;
 
     if (!file.is_open()) {
         cerr << "Failed to open file: " << file_path << endl;
-        return arr;
+        return {arr1, arr2};
     }
 
+    bool flag = true;
     string line;
 
     while (getline(file, line)) {
-        vector<string>t;
-        stringstream ss(line);
-        string x;
-        while(ss>>x) t.push_back(x);
-        arr.push_back(t);
+        if(line.length()==0) {
+            flag=!flag;
+            continue;
+        }
+        if(flag) arr1.push_back(line);
+        else arr2.push_back(line);
     }
     
     file.close();
-    return arr;
+    return {arr1, arr2};
 }
 
-void solve(vector<vector<string>>&arr) {
+void solve(vector<string>grid1, vector<string>grid2) {
     cout<<"\nRES:: "<<""<<"\n\n";
     return;
 }
 
 int main() {
-    vector<vector<string>> arr = get_input("09");
-    solve(arr);
+    pair<vector<string>,vector<string>> p = get_input("09");
+    solve(p.first, p.second);
     return 0;
 }

e61b87e08e93ed0c70cc6780a4df28c93a0fac96
Utkarsh Kumawat <utkarsh2600@gmail.com>
2025-05-03 17:48:01 +0530

    d09

--- 2024/09.cpp
+++ 2024/09.cpp
@@ -6,15 +6,15 @@
 
 using namespace std;
 
-pair<vector<string>,vector<string>> get_input(string file_name) {
+vector<vector<string>> get_input(string file_name) {
     string file_path = "2024/"+file_name+".in";
     ifstream file(file_path);
 
-    vector<string>arr1, arr2;
+    vector<vector<string>>arr(2);
 
     if (!file.is_open()) {
         cerr << "Failed to open file: " << file_path << endl;
-        return {arr1, arr2};
+        return arr;
     }
 
     bool flag = true;
@@ -25,21 +25,25 @@ pair<vector<string>,vector<string>> get_input(string file_name) {
             flag=!flag;
             continue;
         }
-        if(flag) arr1.push_back(line);
-        else arr2.push_back(line);
+        if(flag) arr[0].push_back(line);
+        else arr[1].push_back(line);
     }
     
     file.close();
-    return {arr1, arr2};
+    return arr;
 }
 
-void solve(vector<string>grid1, vector<string>grid2) {
+void solve(vector<vector<string>>grid) {
+    for(vector<string>floor: grid) {
+        for(string row: floor) cout<<row<<"\n";
+        cout<<"\n";
+    }
     cout<<"\nRES:: "<<""<<"\n\n";
     return;
 }
 
 int main() {
-    pair<vector<string>,vector<string>> p = get_input("09");
-    solve(p.first, p.second);
+    vector<vector<string>> arr = get_input("09");
+    solve(arr);
     return 0;
 }
